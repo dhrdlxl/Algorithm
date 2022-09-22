f7e5f7fa9896012bc3d4582ece134c11e3580fd7
SeungHyunYoo <dhrdlxl@naver.com>
2022-09-23 02:48:51 +0900

    [level 2] Title: 순위 검색, Time: 919.72 ms, Memory: 223 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.java
@@ -1,66 +1,83 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 class Solution {
-    private HashMap<String, ArrayList<Integer>> map = new HashMap<>();
-    
     public int[] solution(String[] info, String[] query) {
+        String[] langs = {"cpp", "java", "python"},
+                 deps = {"backend", "frontend"},
+                 exps = {"junior", "senior"},
+                 foods = {"chicken", "pizza"};
+
+        HashMap<String, HashMap<String, HashMap<String, HashMap<String, ArrayList<Integer>>>>> langMap = new HashMap();
+        for (String lang : langs)
+            langMap.put(lang, new HashMap<>());
+        for (String lang : langMap.keySet()) {
+            HashMap<String, HashMap<String, HashMap<String, ArrayList<Integer>>>> depMap = langMap.get(lang);
+            for (String dep : deps)
+                depMap.put(dep, new HashMap<>());
+            for (String dep : depMap.keySet()) {
+                HashMap<String, HashMap<String, ArrayList<Integer>>> expMap = depMap.get(dep);
+                for (String exp : exps)
+                    expMap.put(exp, new HashMap<>());
+                for (String exp : expMap.keySet()) {
+                    HashMap<String, ArrayList<Integer>> foodMap = expMap.get(exp);
+                    for (String food : foods)
+                        foodMap.put(food, new ArrayList<Integer>());
+                }
+            }
+        }
+
+        for (String row : info) {
+            String[] parse = row.split(" ");
+            langMap.get(parse[0]).get(parse[1]).get(parse[2]).get(parse[3]).add(Integer.parseInt(parse[4]));
+        }
+
+        for (HashMap<String, HashMap<String, HashMap<String, ArrayList<Integer>>>> depMap : langMap.values())
+            for (HashMap<String, HashMap<String, ArrayList<Integer>>> expMap : depMap.values())
+                for (HashMap<String, ArrayList<Integer>> foodMap : expMap.values())
+                    for (ArrayList<Integer> scoreList : foodMap.values())
+                        scoreList.sort(null);
+
         int[] answer = new int[query.length];
-        
-        for (String conditions : info) {
-            String[] condition = conditions.split(" ");
-            
-            makeSentence(0, condition, "");
+        int i = 0;
+        for (String quest : query) {
+            String[] parse = quest.split(" and "),
+                     subParse = parse[3].split(" ");
+            parse[3] = subParse[0];
+            int minScore = Integer.parseInt(subParse[1]);
+
+            for (String lang : langs)
+                if (parse[0].equals("-") || parse[0].equals(lang)) {
+                    HashMap<String, HashMap<String, HashMap<String, ArrayList<Integer>>>> depMap = langMap.get(lang);
+                    for (String dep : deps)
+                        if (parse[1].equals("-") || parse[1].equals(dep)) {
+                            HashMap<String, HashMap<String, ArrayList<Integer>>> expMap = depMap.get(dep);
+                            for (String exp : exps)
+                                if (parse[2].equals("-") || parse[2].equals(exp)) {
+                                    HashMap<String, ArrayList<Integer>> foodMap = expMap.get(exp);
+                                    for (String food : foods)
+                                        if (parse[3].equals("-") || parse[3].equals(food)) {
+                                            ArrayList<Integer> scoreList = foodMap.get(food);
+                                            int n = scoreList.size(),
+                                                min = 0,
+                                                max = n-1,
+                                                mid = max,
+                                                idx = n;
+                                            while (min <= max) {
+                                                mid = (min + max) / 2;
+                                                if (scoreList.get(mid) >= minScore) {
+                                                    idx = mid;
+                                                    max = mid - 1;
+                                                } else
+                                                    min = mid + 1;
+                                            }
+                                            answer[i] += n - idx;
+                                        }
+                                }
+                        }
+                }
+            i++;
         }
-        
-        for (String sentence : map.keySet()) {
-            Collections.sort(map.get(sentence));
-        }
-            
-        for (int t = 0; t < query.length; t++) {
-            String[] condition = query[t].replaceAll(" and ", "").split(" ");
-            String sentence = condition[0];
-            int score = Integer.parseInt(condition[1]);
-            
-            if (!map.containsKey(sentence)) {
-                answer[t] = 0;
-            } else {
-                int start = getStartIndex(map.get(sentence), score);
-                answer[t] = map.get(sentence).size() - start;
-            }
-        }
-        
         return answer;
-    }
-    
-    public void makeSentence(int step, String[] condition, String sentence) {
-        if (step == 4) {
-            int score = Integer.parseInt(condition[4]);
-            map.putIfAbsent(sentence.toString(), new ArrayList<>());
-            map.get(sentence.toString()).add(score);
-            return;
-        }
-        //1. -
-        makeSentence(step + 1, condition, sentence + "-");
-        
-        //2. word
-        makeSentence(step + 1, condition, sentence + condition[step]);
-    }
-    
-    //BinearySearch(lowerbound)
-    public int getStartIndex(ArrayList<Integer> arr, int target) {
-        int left = 0;
-        int right = arr.size() - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (arr.get(mid) < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        
-        return left;
     }
 }
