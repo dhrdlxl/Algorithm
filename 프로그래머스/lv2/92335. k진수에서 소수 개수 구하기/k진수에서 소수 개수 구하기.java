e77a74ab7d757ba1939b9c66b34cf700857557ac
SeungHyunYoo <dhrdlxl@naver.com>
2022-09-23 15:45:39 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.15 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,41 +1,45 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
         
-        // 1. k진수 변경
-        StringBuilder sb = new StringBuilder();
+        //1. 진수 표현
+        StringBuilder makeNum = new StringBuilder();
         while (n > 0) {
-            sb.append((char)('0' + n % k));
+            makeNum.append(n % k);
             n /= k;
         }
+        String num = makeNum.reverse().toString();
         
-        String kDigit = sb.reverse().toString();
-        
-        
-        // 2. 0을 기준으로 소수 판단
-        StringTokenizer st = new StringTokenizer(kDigit, "0");
-        while (st.hasMoreTokens()) {
-            long num = Long.parseLong(st.nextToken());
-            if (isPrime(num)) {
-                answer++;
-            }
-        }
-        
+        answer = countPrime(num);
         
         return answer;
     }
     
+    private int countPrime(String n) {
+        String[] nums = n.split("0+");
+        int count = 0;
+        
+        for (String numString : nums) {
+            long num = Long.parseLong(numString);
+            
+            if (isPrime(num)) {
+                count++;
+            }
+        }
+            
+        return count;
+    }
+    
+    //2. 소수 판별
     private boolean isPrime(long num) {
         if (num < 2)
             return false;
         
-        for (long i = 2; i * i <= num; i++) {
-            if (num % i == 0)
+        for (long i = 2; (i * i) <= num; i++) {
+            if (num % i == 0) {
                 return false;
+            }
         }
-        
         return true;
     }
 }
